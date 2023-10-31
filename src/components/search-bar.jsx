35e558fad8fb887e9d3952e38251929f4b4eda9c
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-10-31 16:02:39 -0700

    Add temporary search bar styling

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,7 +1,14 @@
+import '../styles/search-bar.css'
+
+import searchButton from '../assets/search-bar/search-icon.png'
+
 export default function SearchBar() {
   return (
-    <div className="search-bar">
-      <input type="text" />
+    <div className="header">
+      <div className="search-bar-container">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit"><img src={searchButton} alt="search icon" /></button>
+      </div>
     </div>
   )
 }
