b7518c5ec7ab266f0cd211486d30ebb9e008a4e1
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-10-31 16:30:40 -0700

    Add temp=display component

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,9 +1,11 @@
+import TempDisplay from './temp-display'
+
 import '../styles/display.css'
 
 export default function Display() {
   return (
     <div className="main-display">
-      
+      <TempDisplay />
     </div>
   )
 }
---
+++ src/components/temp-display.jsx
@@ -0,0 +1,9 @@
+import '../styles/temp-display.css'
+
+export default function TempDisplay() {
+  return (
+    <div className="temp-display">
+      60
+    </div>
+  )
+}
