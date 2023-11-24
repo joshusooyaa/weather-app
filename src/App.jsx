eb9516ae6c4b70f35080415729e43921e0b0d3d3
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-24 15:13:01 -0800

    Move useEffect to App from Display
    Point of lifting state up here allows for passing in a background
    state to the background based on what the api fetches. This will
    allow for "Sunny", for example, to give us a sunny background.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
 import Background from './components/background'
 import SearchBar from './components/search-bar'
 import Display from './components/display'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 
 function App() {
   const [location, setLocation] = useState({lat: 51.5072, long: 0.1276});
 
+  const [tempF, setTempF] = useState(null)
+  
+  const apiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
+  
+  // Gather geolocation on load, then set location based on that
+  useEffect(() => {
+    const handleSuccess = (position) => {
+      setLocation({lat: position.coords.latitude, long: position.coords.longitude})
+    }
+  
+    const handleError = (error) => {
+      console.warn(error)
+    }
+  
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+    }
+    else {
+      console.log("Geolocation not available")
+    }
+  }, [])
+
+  useEffect(() => {
+    const fetchWeather = (lat, long) => {
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat} ${long}&aqi=no`, {mode: 'cors'})
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setTempF(data.current.temp_f)
+      })
+      .catch(error => {
+        console.warn(error)
+      })
+    }
+
+    fetchWeather(location.lat, location.long)
+  }, [location.lat, location.long]);
   return (
     <>
       <Background />
-      <SearchBar location={location} setLocation={setLocation}/>
-      <Display location={location} setLocation={setLocation}/>
+      <SearchBar location={location}/>
+      <Display tempF={tempF}/>
     </>
   )
 }
--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,51 +1,10 @@
 import TempDisplay from './temp-display'
 import ForecastDisplay from './forecast-display'
-
-import { useEffect, useState } from 'react'
-
 import PropTypes from 'prop-types'
 
 import '../styles/display.css'
 
-export default function Display( { location, setLocation }) {
-  const [tempF, setTempF] = useState(null)
-  
-  const apiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
-  
-  useEffect(() => {
-    const handleSuccess = (position) => {
-      setLocation({lat: position.coords.latitude, long: position.coords.longitude})
-    }
-  
-    const handleError = (error) => {
-      console.warn(error)
-    }
-  
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
-    }
-    else {
-      console.log("Geolocation not available")
-    }
-  }, [])
-
-  useEffect(() => {
-    const fetchWeather = (lat, long) => {
-      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat} ${long}&aqi=no`, {mode: 'cors'})
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        setTempF(data.current.temp_f)
-      })
-      .catch(error => {
-        console.warn(error)
-      })
-    }
-
-    fetchWeather(location.lat, location.long)
-  }, [location.lat, location.long]);
-
+export default function Display( { tempF }) {
   return (
     <div className="main-display">
       <TempDisplay tempF={tempF}/>
@@ -55,9 +14,5 @@ export default function Display( { location, setLocation }) {
 }
 
 Display.propTypes = {
-  location: PropTypes.shape({
-    lat: PropTypes.number,
-    long: PropTypes.number
-  }),
-  setLocation: PropTypes.func,
+  tempF: PropTypes.number,
 }
