3a9ff308c295bbd8a3853882c8445a77a7853925
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-24 14:10:23 -0800

    Add api call for display - current

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Background from './components/background'
 import SearchBar from './components/search-bar'
 import Display from './components/display'
+import { useState } from 'react'
 
 import './App.css'
 
 function App() {
+  const [location, setLocation] = useState({lat: 51.5072, long: 0.1276});
+
   return (
     <>
       <Background />
-      <SearchBar />
-      <Display />
+      <SearchBar location={location} setLocation={setLocation}/>
+      <Display location={location} setLocation={setLocation}/>
     </>
   )
 }
--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,13 +1,61 @@
 import TempDisplay from './temp-display'
 import ForecastDisplay from './forecast-display'
 
+import { useEffect } from 'react'
+
+import PropTypes from 'prop-types'
+
 import '../styles/display.css'
 
-export default function Display() {
+export default function Display( { location, setLocation }) {
+  const apiKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
+  
+  useEffect(() => {
+    const handleSuccess = (position) => {
+      setLocation({lat: position.coords.latitude, long: position.coords.longitude})
+    }
+  
+    const handleError = (error) => {
+      console.warn(error)
+    }
+  
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+    }
+    else {
+      console.log("Geolocation not available")
+    }
+  }, [])
+
+  useEffect(() => {
+    const fetchWeather = (lat, long) => {
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat} ${long}&aqi=no`, {mode: 'cors'})
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.warn(error)
+      })
+    }
+
+    fetchWeather(location.lat, location.long)
+  }, [location.lat, location.long]);
+
   return (
     <div className="main-display">
       <TempDisplay />
       <ForecastDisplay />
     </div>
   )
+}
+
+Display.propTypes = {
+  location: PropTypes.shape({
+    lat: PropTypes.number,
+    long: PropTypes.number
+  }),
+  setLocation: PropTypes.func,
 }
