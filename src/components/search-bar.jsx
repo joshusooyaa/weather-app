7c0e17187b2fe3b0d98ee180bae952004b2711ef
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-24 16:27:15 -0800

    Add autocomplete api for search

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import '../styles/search-bar.css'
 
@@ -7,6 +7,42 @@ import searchButton from '../assets/search-bar/search-icon.png'
 export default function SearchBar() {
   const [ input, setInput ] = useState("")
   
+  const weatherKey = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
+  const locationKey = import.meta.env.VITE_REACT_APP_LOCATIONIQ_API_KEY
+
+  const displayAddress = (address) => {
+    let primary = address.city || address.town || address.village || address.hamlet;
+    let secondary = address.state || address.region;
+    let final = address.postcode || address.country;
+
+    let formatted = [];
+    if (primary) formatted.push(primary);
+    if (secondary) formatted.push(secondary);
+    if (final) formatted.push(final);
+
+    return formatted.join(", ")
+  }
+
+  useEffect(() => {
+    const apiTimeout = setTimeout(() => {
+      fetch(`https://api.locationiq.com/v1/autocomplete?key=${locationKey}&q=${input}&limit=6`, {mode: 'cors'})
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          data.forEach((place) => {
+            console.log(displayAddress(place.address))
+          })
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }, 500)
+    
+    return () => clearTimeout(apiTimeout);
+
+  }, [ input ] )
+
   const handleInput = (e) => {
     setInput(e.target.value);
   }
