6a30a26f14fd6d4248139588db3d105065d39b94
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-30 14:18:29 -0800

    Set up hourly forecast template

--- src/components/forecast-display.jsx
+++ src/components/forecast-display.jsx
@@ -4,7 +4,18 @@ import '../styles/forecast-display.css'
 export default function ForecastDisplay() {
   return (
     <div className="forecast-display">
-
+      <div className="hourly-forecast-module">
+        <p className="hour-title">Hourly Forecast</p>
+        <div className="hour-section-container">
+          <div className="hour-section">
+            <p className="hour-time">Now</p>
+            <div className='img-container'>
+              <img className="hour-image" src="//cdn.weatherapi.com/weather/64x64/night/143.png" alt="" />
+            </div>
+            <p className="hour-temp">5*</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
